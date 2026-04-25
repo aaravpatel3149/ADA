a12290e5e21f63fddbfe52078dc68a237d14841c
aaravpatel3149 <aaravpatel3149@gmail.com>
2026-04-25 11:33:05 +0530

    Create p7.java

---
+++ experiment/p7.java
@@ -0,0 +1,46 @@
+public class Prim {
+    static int V = 5;
+
+    int minKey(int[] key, boolean[] mst) {
+        int min = Integer.MAX_VALUE, index = -1;
+        for (int i=0;i<V;i++)
+            if (!mst[i] && key[i] < min) {
+                min = key[i]; index = i;
+            }
+        return index;
+    }
+
+    void primMST(int[][] graph) {
+        int[] parent = new int[V];
+        int[] key = new int[V];
+        boolean[] mst = new boolean[V];
+
+        for (int i=0;i<V;i++) key[i] = Integer.MAX_VALUE;
+        key[0] = 0; parent[0] = -1;
+
+        for (int i=0;i<V-1;i++) {
+            int u = minKey(key, mst);
+            mst[u] = true;
+
+            for (int v=0;v<V;v++)
+                if (graph[u][v]!=0 && !mst[v] && graph[u][v]<key[v]) {
+                    parent[v] = u;
+                    key[v] = graph[u][v];
+                }
+        }
+
+        for (int i=1;i<V;i++)
+            System.out.println(parent[i]+" - "+i);
+    }
+
+    public static void main(String[] args) {
+        int[][] graph = {
+            {0,2,0,6,0},
+            {2,0,3,8,5},
+            {0,3,0,0,7},
+            {6,8,0,0,9},
+            {0,5,7,9,0}
+        };
+        new Prim().primMST(graph);
+    }
+}
